15185443250584c8ff9064645447730d2526d0e8
cooperj157 <cooperj157@gmail.com>
2022-07-29 23:47:09 -0400

    can see all contracts as a list and release whichever you choose

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import { ethers } from 'ethers';
 import Escrow from './artifacts/contracts/Escrow.sol/Escrow';
 import Contracts from './Contracts';
 
-
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 let contractAddr = '';
-
+let counter = 1;
 function App() {
   
   const [inputText, changeText] = useState(['','',0]);
-  const [contract, newContract] = useState(["Once you create a contract it will be placed below",{
+  const [contract, newContract] = useState([{
     display: 'none'
   }]);
-  
-  
+
   function handleArbiterChange(event){
     changeText([event.target.value,inputText[1],inputText[2]]);
   }
@@ -33,7 +31,6 @@ function App() {
     const beneficiary = '0xCd55Cf96929064a924EEE9E2Ea53d802b5C7DcD9';
     const value = "100000000000000000";
     changeText([arbiter,beneficiary,value]);
-    
   }
 
   async function createContract(){
@@ -42,35 +39,46 @@ function App() {
     const _value = inputText[2];
     contractAddr = await deploy(_arbiter, _beneficiary, _value);
 
-    newContract(["Your contract:",{
+    newContract([...contract,{
+      id: counter,
       arbiter: 'Arbiter: '+_arbiter,
       beneficiary: 'Beneficiary: '+_beneficiary,
-      address: 'Address: '+ contractAddr,
+      address: contractAddr,
       value: 'Value: '+ _value,
       releaseMsg: 'Release the funds',
       status: "Status: Active",
       disabled: false
     }]);
-    
+    counter++;
   }
 
-  async function releaseFunds(){
-    const thisContract = new ethers.Contract(contractAddr,Escrow.abi,provider);
-    const _arbiter = contract[1].arbiter;
-    const _beneficiary = contract[1].beneficiary;
-    const _address = contract[1].address;
-    const _value = contract[1].value;
+  async function releaseFunds(id){
 
+    const _address = contract[id].address;
+    const thisContract = new ethers.Contract(_address,Escrow.abi,provider);
+
+    const _id = contract[id].id;
+    const _arbiter = contract[id].arbiter;
+    const _beneficiary = contract[id].beneficiary;
+    const _value = contract[id].value;
+
+    let contractsUpToID = contract.slice(0,id);
+    let contractsAfterID = null;
+    if(counter > id){
+      contractsAfterID = contract.slice(id+1);
+    }
+    
     await thisContract.connect(provider.getSigner(0)).release();
-    newContract(["Your contract:",{
+    newContract([...contractsUpToID, {
+      id: _id,
       arbiter: _arbiter,
       beneficiary: _beneficiary,
       address: _address,
       value: _value,
       releaseMsg: 'Released',
-      status: "Status: Funds have been released - inactive",
+      status: "Status: Funds have been released, now inactive",
       disabled: true
-    }]);
+    },...contractsAfterID]);
     
   }
 
@@ -86,10 +94,10 @@ function App() {
         <button id='createButton' onClick={createContract}>Create Contract</button>
         <button id='setAddresses' onClick={setAddresses}>fill the fields with addresses</button>
       </div>
-      <div>
-        <Contracts contract={contract} onClick={releaseFunds}/>
-      </div>
-        
+      <div className="contracts">
+        <div>Your Contracts</div>
+        {contract.map((contr, i)=> <Contracts contract={contr} key={i} onClick={async()=>releaseFunds(contr.id)}/>)}
+      </div>   
     </div>
       
   );
--- src/Contracts.js
+++ src/Contracts.js
@@ -4,18 +4,15 @@ export default function Contracts(props){
     
     return (
         <div>
-            <div>
-                <div>{props.contract[0]}</div>
-            </div>
             <div className={'contract'}>
-                <div>{props.contract[1].arbiter}</div>
-                <div>{props.contract[1].beneficiary}</div>
-                <div>{props.contract[1].address}</div>
-                <div>{props.contract[1].value}</div>
-                <div>{props.contract[1].status}</div>
-                <button onClick={props.onClick} disabled={props.contract[1].disabled} style={{display: props.contract[1].display}}>{props.contract[1].releaseMsg}</button>
+                <div>{props.contract.id}</div>
+                <div>{props.contract.arbiter}</div>
+                <div>{props.contract.beneficiary}</div>
+                <div>{props.contract.address}</div>
+                <div>{props.contract.value}</div>
+                <div>{props.contract.status}</div>
+                <button onClick={props.onClick} disabled={props.contract.disabled} style={{display: props.contract.display}}>{props.contract.releaseMsg}</button>
             </div>
-
         </div>
         
         
